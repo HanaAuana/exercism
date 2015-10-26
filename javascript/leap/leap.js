f8adb0f884b691c93d9f56a7abf765528bd94faf
Michael Lim <michaellim@surveymonkey.com>
2015-10-26 13:35:06 -0700

    Complete Javascript ex2

---
+++ javascript/leap/leap.js
@@ -0,0 +1,23 @@
+
+var LeapYear = function() {};
+
+ function isLeapYear(year){
+  if(year % 4 === 0){
+    if( year % 100 === 0){
+      if(year % 400 === 0){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    else{
+      return true;
+    }
+  }
+  else{
+    return false;
+  }
+}
+
+module.exports = isLeapYear;
