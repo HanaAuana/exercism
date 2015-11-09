178757c2b3a665053b71630cbc4256df320be8c9
Michael Lim <michaellim@surveymonkey.com>
2015-11-09 07:31:30 -0800

    Simplify nested if/else

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -2,21 +2,16 @@
 var LeapYear = function() {};
 
  function isLeapYear(year){
-  if(year % 4 === 0){
-    if( year % 100 === 0){
-      if(year % 400 === 0){
-        return true;
-      }
-      else{
-        return false;
-      }
-    }
-    else{
-      return true;
-    }
+  if(year % 4 === 0 && year % 100 !== 0){
+    return true;
   }
   else{
-    return false;
+    if( year % 100 === 0 && year % 400 === 0){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 }
 
